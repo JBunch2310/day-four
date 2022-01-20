2fadcaeb2d64147105bdc32022f6cff12cf82123
Jamie Bunch <jamiebunch@Jamies-MacBook-Air.local>
2022-01-20 14:52:53 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function readTheFile(){
+// function readsTheData(){
 
-const fs = require('fs')
-const data = fs.readFileSync('data-json.json','utf-8')
-return data
+// const fs = require('fs')
+// const data = fs.readFileSync('data.json',"utf-8")
+// return data
+// }
+// console.log(readsTheData())
+
+function readsTheData(){
+
+    const fs = require('fs')
+    fs.readFile('data.json', (err, data) => {
+      if (err) throw err 
+      console.log(data.toString("utf-8"))
+})
 }
-console.log(readTheFile())
+readsTheData()
